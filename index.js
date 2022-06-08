4a054c35fbe71f9926e9d1ccbdfb0ff56dbee885
william-prestes <william.prestes99@gmail.com>
2022-06-07 21:25:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function somar(numero1, numero2){
+    return numero1 + numero2
+}
+let resultadoSoma = somar(6, 2)
+console.log(resultadoSoma)
+
+function dividir(numero1, numero2){
+    return numero1 / numero2
+}
+let resultadoDivisao = dividir(8, 2)
+console.log(resultadoDivisao)
+
+function multiplicar(numero1, numero2){
+    return numero1 * numero2
+}
+let resultadoMultiplicacao = multiplicar(3, 5)
+console.log(resultadoMultiplicacao)
+
+function subtrair(numero1, numero2){
+    return numero1 - numero2
+}
+let resultadoSubtracao = subtrair(7, 2)
+console.log(resultadoSubtracao)
